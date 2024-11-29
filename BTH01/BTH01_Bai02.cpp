71e6203b8b604a6cad6643a78ea705f3838d3e93
Phan An Phú <168607990+kog498@users.noreply.github.com>
2024-11-29 20:04:23 +0700

    first commit

---
+++ BTH01/BTH01_Bai02.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cout << "****************************" << endl;
+	cout << "*      Bai thuc hanh: 1    *" << endl;
+	cout << "*      Mon: CSLT           *" << endl;
+	cout << "*      MSSV: 2451052196    *" << endl;
+	cout << "****************************";
+	return 0;
+}
---
+++ BTH01/BTH01_Bai06.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	cout << "Xin chao, ten toi la Hoa!" << endl;
+	cout << "Ten ban la gi? ";
+	string username;
+	cin >> username;
+	cout << "Chao ban, " << username;
+	return 0;
+}
